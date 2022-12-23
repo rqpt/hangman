efb7984c38642fe49fa6b9a3376cfb6d8a0fcf52
rqpt <1ernstvermeulen1@gmail.com>
2022-12-23 10:16:07 +0200

    refactored in a oo way

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,20 +1,41 @@
-dictionary = File.read('word_list.txt').split($/).filter { |w| w.length.between?(5, 12) }
-secret_word = dictionary.sample
+class Game
+  @@dictionary = File.read('word_list.txt').split($/).filter { |w| w.length.between?(5, 12) }
 
-guesses_left = 10
-broken_word = Array.new(secret_word.length, '_')
-secret_word_arr = secret_word.split('')
+  def initialize
+    @secret_word = @@dictionary.sample
+    @guesses_left = 10
+    @broken_word = Array.new(@secret_word.length, '_')
+    @secret_word_arr = @secret_word.split('')
+  end
 
-until guesses_left.zero?
-  puts "You have #{guesses_left} guesses left, guess a letter"
-  puts "            #{broken_word.join(' ')}"
-  guess = gets.chomp
+  def play_game
+    until @guesses_left.zero?
+      display_game_state
+      handle_player_input
 
-  secret_word_arr.each_with_index { |c, i| broken_word[i] = guess if c == guess }
-  guesses_left -= 1
+      @guesses_left -= 1
+      break if @secret_word_arr == @broken_word
+    end
+  end
 
-  break if secret_word_arr == broken_word
+  private
+
+  def display_game_state
+    puts "You have #{@guesses_left} guesses left, guess a letter"
+    puts "            #{@broken_word.join(' ')}"
+  end
+
+  def handle_player_input
+    @guess = gets.chomp.downcase
+    @secret_word_arr.each_with_index { |c, i| @broken_word[i] = @guess if c == @guess }
+  end
+
+  def display_result
+    puts @secret_word_arr == @broken_word ? 'You win!' : 'You lose!'
+    puts "The secret word was #{@secret_word}"
+  end
 end
 
-puts secret_word_arr == broken_word ? 'You win!' : 'You lose!'
-puts "The secret word was #{secret_word}"
+game = Game.new
+game.play_game
+game.display_result
